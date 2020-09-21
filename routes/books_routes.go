4309128247014a1050a0011fc98896321e55de25
JaimeRC <jaime.rc@icloud.com>
2020-09-21 21:01:53 +0200

    Add routes

---
+++ routes/books_routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"go-rollercoaster-api/controllers"
+)
+
+// SetContactsRoutes agrega las rutas de contactos
+func SetBooksRoutes(r *mux.Router) {
+
+	subRouter := r.PathPrefix("/api").Subrouter()
+
+	// Route handles & endpoints
+	subRouter.HandleFunc("/books", controllers.GetBooks).Methods("GET")
+	subRouter.HandleFunc("/books/{id}", controllers.GetBook).Methods("GET")
+	subRouter.HandleFunc("/books", controllers.CreateBook).Methods("POST")
+	subRouter.HandleFunc("/books/{id}", controllers.UpdateBook).Methods("PUT")
+	subRouter.HandleFunc("/books/{id}", controllers.DeleteBook).Methods("DELETE")
+}
---
+++ routes/health_check_routes.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"go-rollercoaster-api/models"
+	"net/http"
+)
+
+
+
+func SetHealthCheckRoutes(r *mux.Router) {
+
+	// Route handles & endpoints
+	r.HandleFunc("/health_check", getHealthCheck).Methods("GET")
+}
+
+// Get all books
+func getHealthCheck(w http.ResponseWriter, r *http.Request) {
+	var response models.Status
+	response.Status = true
+	response.Message = "Server is running!!!"
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
