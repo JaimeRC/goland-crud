7a6433e2e59b6957166a01640c1d8ffb02e65980
JaimeRC <jaime.rc@icloud.com>
2020-09-27 21:34:34 +0200

    Add MongoDB Connection

--- database/mongodb.go
+++ database/mongodb.go
@@ -1,1 +1,37 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"go-rollercoaster-api/controllers"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
+)
+
+
+func Connect(host string) {
+	// Database Config
+	clientOptions := options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:port",host))
+	client, err := mongo.NewClient(clientOptions)
+
+	//Set up a context required by mongo.Connect
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+
+	//To close the connection at the end
+	defer cancel()
+
+	err = client.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal("Couldn't connect to the database", err)
+	} else {
+		log.Println("Connected!")
+	}
+
+	db := client.Database("apiBook")
+
+	controllers.BookCollection(db)
+}
